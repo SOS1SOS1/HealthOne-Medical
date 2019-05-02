898bfa0dec25633b78e2b1c22de9ca0121862322
SOS1SOS1 <shanti.mickens@mtchs.org>
2019-05-02 10:44:21 -0600

    new commit

--- home.php
+++ home.php
@@ -1,22 +1,27 @@
-<?php
 
-    session_start();
-    // checks if session variable user is set, if they logged in
-    if (!isset($_SESSION['user'])) {
-        header('Location: login.php');
-        //exit();
-    }
-    echo '<br>';
-    echo $_SESSION['user'];
-    echo '<a href="logout.php"> Logout </a></br>';
-?>
 
 <!DOCTYPE html>
 <html>
   <head>
+
     <title> Home Page </title>
     <link rel="stylesheet" href="main.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+    <?php
+
+        session_start();
+        // checks if session variable user is set, if they logged in
+        if (!isset($_SESSION['user'])) {
+            header('Location: login.php');
+            //exit();
+        }
+        echo '<br>';
+        echo '<div id="homeLogout">';
+        echo $_SESSION['user'];
+        echo '<br>';
+        echo '<a href="logout.php"> Logout </a></br>';
+        echo '</div>';
+    ?>
   </head>
   <body>
     <h1>HealthOne Medical</h1>
@@ -28,13 +33,11 @@
 
 <?php
 
-
     require_once('/moredata/shantim/etc/mysqli_connect_medical.php');
 
     $q = "SELECT * FROM PATIENT";
     $r = mysqli_query($dbc,$q);
     $results = mysqli_fetch_array($r, MYSQLI_ASSOC);
-
 
     echo '<table style="width:50%">';
     foreach($r as $row){
