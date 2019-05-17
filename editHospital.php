0f87b6781e0c631b55badcf1b777b369eeb5e3b2
Tia Flores <tia.flores-carr@mtchs.org>
2019-05-17 09:52:35 -0600

    Update editHospital.php

--- editHospital.php
+++ editHospital.php
@@ -26,13 +26,12 @@
     # checks that there is an id and that it is a number
     if (isset($_GET['id']) && is_numeric($_GET['id'])) {  // from table.php
         $id = $_GET['id'];
-    } else if (isset($_GET['id_doc']) && is_numeric($_GET['id_doc'])) {  // from table.php
+    }
+    if (isset($_GET['id_doc']) && is_numeric($_GET['id_doc'])) {  // from table.php
         $id_doc = $_GET['id_doc'];
-    } else if (isset($_GET['id_pat']) && is_numeric($_GET['id_pat'])) {  // from table.php
+    }
+    if (isset($_GET['id_pat']) && is_numeric($_GET['id_pat'])) {  // from table.php
         $id_pat = $_GET['id_pat'];
-    } else {
-        echo '<p> This page has been accessed in error. </p>';
-        exit();
     }
 
     // requires that we are able to connect to the database using are hidden php file
@@ -44,12 +43,12 @@
     $row_hospital = mysqli_fetch_array($r_hos, MYSQLI_ASSOC);
 
     echo '<form action = "editHospital.php?id=' . $id . '&id_doc=' . $id_doc . '&id_pat=' . $id_pat . '" method = "post">';
-        echo '<h3>Name: <input type = "text" name = "name" size = "40" maxlength="40" value="' . $row_hospital['name'] . '"></h3>';
+        echo '<h3>Name: <input type = "text" name = "name" size = "15" maxlength="30" value="' . $row_hospital['name'] . '"></h3>';
         echo '<h3>Location: <input type = "text" name = "location" size = "30" maxlength="50" value="' . $row_hospital['location'] . '"></h3>';
         echo '<h3>Phone Number: <input type="numbernumber" name = "phoneNumber" pattern="\d*" minLength="10" maxlength="10" value="' . $row_hospital['phoneNumber'] . '"></h3>';
         echo '<input type="submit" name="submit" value="Update Hospital" id="submit"><br><br>';
     echo '</form>';
-    echo '<a href="hospital.php?id=' . $id . '">Cancel</a>';
+    echo '<a href="hospital.php?id_hos=' . $id . '&id_doc=' . $id_doc . '&id_pat=' . $id_pat . '">Cancel</a>';
 
     # checks that the form was submitted
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -80,9 +79,6 @@
 
         if (empty($errors)) {
 
-          echo 'test';
-          echo $id;
-
             // updates patient information
             $q = "UPDATE HOSPITAL SET name = '$name', location = '$location', phoneNumber = '$phoneNum' WHERE hospital_id = $id";
             $r = @mysqli_query($dbc, $q);
@@ -99,8 +95,8 @@
         }
 
         // redirects user back to client page
-        $hospital_page = "http://shantim.smtchs.org/HealthOne_Medical/hospital.php?id_hos=" . $id;
-      //  echo "<script type='text/javascript'>window.top.location='$hospital_page';</script>"; exit;
+        $hospital_page = "http://shantim.smtchs.org/HealthOne_Medical/hospital.php?id_doc=" . $id_doc ."&id_hos=" . $id . "&id_pat=" . $id_pat;
+        echo "<script type='text/javascript'>window.top.location='$hospital_page';</script>"; exit;
 
     }
 
