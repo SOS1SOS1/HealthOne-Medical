0ec726724f7123549db4e8fc00633b93d384e2cc
Tia Flores <tia.flores-carr@mtchs.org>
2019-05-17 11:09:55 -0600

    :neckbeard:

--- addNewDoctor.php
+++ addNewDoctor.php
@@ -4,10 +4,10 @@
     include('header.php');
 
     // shows new doctor form
-    function createDoctorForm($r_hospital, $row_doctor, $id_p) {
-        echo '<form action = "addDoctor.php" method = "post">';
-            echo '<label>First Name</label><input type="text" name="docFirst" value = ' . $row_doctor['firstName'] . '><br>';
-            echo '<label>Last Name</label><input type="text" name="docLast" value = ' . $row_doctor['lastName'] . '><br>';
+    function createDoctorForm($r_hospital) {
+        echo '<form action = "addNewDoctor.php" method = "post">';
+            echo '<label>First Name</label><input type="text" name="docFirst"><br>';
+            echo '<label>Last Name</label><input type="text" name="docLast"><br>';
             echo '<label>Specialty</label><input type="text" name="docSpecialty" required><br>';
             echo '<label>Phone Number</label><input type="numbernumber" name = "docPhoneNum" pattern="\d*" minLength="10" maxlength="10" required><br>';
             echo '<label>Email</label><input type="email" name="docEmail" required><br>';
@@ -15,27 +15,17 @@
             foreach($r_hospital as $results){
                 echo '<input type="checkbox" name="affiliation[]" value=' . $results['hospital_id'] . '> ' . $results['name'] . ' <br>';
             }
-            echo '<input type="hidden" name = "idPat" value=' . $id_p . '>';
-            echo '<input type="hidden" name = "idDoc" value=' . $row_doctor['doctor_id'] . '>';
             echo '<input type="submit" name="submit" id="submit">';
         echo '</form>';
     }
-    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-        $id = $_GET['id'];
-    } elseif (isset($_POST['id']) && is_numeric($_POST['id'])) {  // from form submission
-        $id = $_POST['id'];
-    }
-    if (isset($_GET['id_pat']) && is_numeric($_GET['id_pat'])) {
-        $id_pat = $_GET['id_pat'];
-    }
+
     # requires that we are able to connect to the database using are hidden php file
     require_once('/moredata/shantim/etc/mysqli_connect_medical.php');
+
     $q = "SELECT * FROM HOSPITAL";
     $r = @mysqli_query($dbc, $q);
-    $q = "SELECT * FROM DOCTOR WHERE doctor_id = $id";
-    $r_doc = @mysqli_query($dbc, $q);
-    $row_doc = mysqli_fetch_array($r_doc, MYSQLI_ASSOC);
-    createDoctorForm($r, $row_doc, $id_pat);
+
+    createDoctorForm($r);
     # checks that the form was submitted
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // errors array
@@ -82,37 +72,54 @@
         } else {
             $affiliation[] = $_POST['affiliation'];
         }
-        // checks for the doctor id
-        if (empty($_POST['idDoc'])) {
-            $errors[] = 'Couldn\'t find doctor\'s id.';
-        } else {
-            $idDoc = $_POST['idDoc'];
-        }
-        // checks for the patient id
-        if (empty($_POST['idPat'])) {
-            $errors[] = 'Couldn\'t find client\'s id.';
-        } else {
-            $idPat = $_POST['idPat'];
-        }
-        // checks if there were no errors
-        if (empty($errors)) {
-            // updates doctor with new information
-            $q = "UPDATE DOCTOR SET firstName = '$first_name', lastName = '$last_name', address = '$address', phoneNumber = '$phone_number', email = '$email', specialty = '$specialty' WHERE doctor_id = $idDoc";
-            $r = @mysqli_query($dbc, $q);
 
-            // redirects user to settings page
-            $settings_page = "http://shantim.smtchs.org/HealthOne_Medical/settings.php";
-            echo "<script type='text/javascript'>window.top.location='$settings_page';</script>"; exit;
-        } else {
-            # reports the errors
-            echo '<p> The following error(s) occured:<br>';
-            foreach ($errors as $msg) {
-                echo " - $msg<br>\n";
+        $q = "SELECT COUNT(doctor_id) FROM DOCTOR where firstName = '$first_name' and lastName = '$last_name' and address = '$address' and phoneNumber = '$phone_number'";
+        $r = @mysqli_query($dbc, $q);
+        $row = mysqli_fetch_array($r, MYSQLI_NUM);
+        $doctors = $row[0];
+
+        // checks if the doctor already exists
+        if ($doctors == 0) {
+
+            // checks if there were no errors
+            if (empty($errors)) {
+                // updates doctor with new information
+                $q = "INSERT INTO DOCTOR (firstName, lastName, address, phoneNumber, email, specialty) VALUES ('$first_name', '$last_name', '$address', '$phone_number', '$email', '$specialty')";
+                $r = mysqli_query($dbc,$q);
+                $results = mysqli_fetch_array($r, MYSQLI_ASSOC);
+
+                $q = "SELECT doctor_id FROM DOCTOR WHERE firstName = '$first_name' and lastName = '$last_name' and address = '$address' and email = '$email' and specialty = '$specialty' and phoneNumber = '$phone_number'";
+                $r = mysqli_query($dbc,$q);
+                $results = mysqli_fetch_array($r, MYSQLI_ASSOC);
+                $idDoc = $results['doctor_id'];
+
+                // adds doctor's affiliations
+                foreach ($_POST['affiliation'] as $hospital_id) {
+                    // adds affiliation
+                    $q = "INSERT INTO AFFILIATION (doctor, hospital) VALUES ('$idDoc', '$hospital_id')";
+                    $r = @mysqli_query($dbc, $q);
+                }
+
+                // redirects user to settings page
+                $settings_page = "http://shantim.smtchs.org/HealthOne_Medical/settings.php";
+                echo "<script type='text/javascript'>window.top.location='$settings_page';</script>"; exit;
+            } else {
+                # reports the errors
+                echo '<p> The following error(s) occured:<br>';
+                foreach ($errors as $msg) {
+                    echo " - $msg<br>\n";
+                }
+                echo '</p><p> Please try again. </p>';
             }
-            echo '</p><p> Please try again. </p>';
+
+        } else {
+
+            echo '<p> The following error(s) occured:<br>';
+            echo " - Doctor already exists.";
+
         }
     }
-    echo "<a href='settings.php'>Go Back</a>";
+    echo "<br><a href='settings.php'>Go Back</a>";
     mysqli_close($dbc);
     include("footer.html");
 ?>
